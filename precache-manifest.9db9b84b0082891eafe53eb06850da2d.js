64a6228fc606fd558d9483dc148d5f6cec479ee8
root <root@kali>
2020-12-20 16:13:17 -0500

    Updates

---
+++ precache-manifest.9db9b84b0082891eafe53eb06850da2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a02ca22fc65bad752c90395d380a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f515e053c3989c86c0",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "da6fd7a02cd995498cb5",
+    "url": "/static/js/2.8e73fc4f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8e73fc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f515e053c3989c86c0",
+    "url": "/static/js/main.36ef1ccf.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2fa765956710ed75c20a04b60d0ae335",
+    "url": "/static/media/hashirshoaeb.2fa76595.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
